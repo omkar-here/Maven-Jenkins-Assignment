1b4c783a23a404be06af9e698d0fd328150009b3
omkar-causalfunnel <omkar@dev.causalfunnel.com>
2024-02-29 01:44:25 +0530

    Second commit

--- src/main/java/com/example/demo/SpringMvcApplication.java
+++ src/main/java/com/example/demo/SpringMvcApplication.java
@@ -8,6 +8,7 @@ public class SpringMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
