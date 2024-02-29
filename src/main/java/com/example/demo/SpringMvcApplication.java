ef96c1ef580ca6ea9d3993aa81db61cc68093c8c
Omkar Anabathula <92844364+omkar-here@users.noreply.github.com>
2024-02-29 10:25:32 +0530

    Update SpringMvcApplication.java

--- src/main/java/com/example/demo/SpringMvcApplication.java
+++ src/main/java/com/example/demo/SpringMvcApplication.java
@@ -9,7 +9,6 @@ public class SpringMvcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcApplication.class, args);
 		System.out.println("Hello World");
-		System.out.println("Jenkins assgn");
 	}
 
 }
